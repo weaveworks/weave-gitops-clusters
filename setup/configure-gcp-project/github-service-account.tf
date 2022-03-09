80ac9d0abde7e4848dcc2a61a03a4c8d62356431
Sam Cook <sam.cook@weave.works>
2022-03-09 14:33:26 +0000

    Allow entire pool to access the github SA
    Currently the pool only has this service account linked to it so this
    doesn't change our risks here. Long term, if further SAs are added we
    should restrict this to maintain least-privilege but until that point
    this means I don't have to figure out how these attributes end up (I
    think `user/repo` should be `weaveworks/weave-gitops` or similar)

--- setup/configure-gcp-project/github-service-account.tf
+++ setup/configure-gcp-project/github-service-account.tf
@@ -27,7 +27,7 @@ module "oidc" {
   sa_mapping = {
     (google_service_account.github_actions_service_account.account_id) = {
       sa_name   = google_service_account.github_actions_service_account.name
-      attribute = "attribute.repository/user/repo"
+      attribute = "*"
     }
   }
 }
