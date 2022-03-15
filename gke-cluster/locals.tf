b8b61f18569087b4da8505b8c46bc372a6ab975b
Sam Cook <sam.cook@weave.works>
2022-03-15 17:29:49 +0000

    Add us-central1 workspace
    Mostly for testing the makefile, not yet deployed

--- gke-cluster/locals.tf
+++ gke-cluster/locals.tf
@@ -8,6 +8,7 @@ locals {
   # Make sure we're in a known workspace
   allowed_workspaces = {
     europe-west1 = true
+    us-central1  = true
   }
   is_allowed_workspace = local.allowed_workspaces[terraform.workspace]
 }
