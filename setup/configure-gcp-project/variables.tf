69959c3b7a1ff2ee8e60b27e454e82f61361610d
Sam Cook <sam.cook@weave.works>
2022-04-01 18:07:55 +0100

    Configure IAP consent screen in tf

--- setup/configure-gcp-project/variables.tf
+++ setup/configure-gcp-project/variables.tf
@@ -3,3 +3,10 @@ variable "project_id" {
   description = "Project Id to deploy into"
   default     = "weave-gitops-clusters"
 }
+
+variable "oauth_support_email" {
+  type        = string
+  description = "Email required for setting up the oAuth consent screen"
+  # FIXME this should be a group email but it has to be one I have admin on
+  default = "sam.cook@weave.works"
+}
