0a47fc3fedffa95fe11e5558c98580fb40570d81
Yiannis <yiannis@weave.works>
2022-09-16 14:39:02 +0100

    feat: Add new leaf cluster

--- gke-cluster/locals.tf
+++ gke-cluster/locals.tf
@@ -7,10 +7,11 @@ locals {
 
   # Make sure we're in a known workspace
   allowed_workspaces = {
-    internal-dev = true
-    europe-west1 = true
-    us-central1  = true
+    internal-dev    = true
+    europe-west1    = true
+    us-central1     = true
     internal-dev-02 = true
+    leaf-02         = true
   }
   is_allowed_workspace = local.allowed_workspaces[terraform.workspace]
 }
