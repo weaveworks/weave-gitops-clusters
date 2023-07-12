082a9a80f0f3dc6105ab02914859d9283ca39d42
Yiannis <8741709+yiannistri@users.noreply.github.com>
2023-07-12 14:32:48 +0100

    Update main.tf

--- k8s/clusters/internal-dev-gke/terraform/main.tf
+++ k8s/clusters/internal-dev-gke/terraform/main.tf
@@ -13,3 +13,12 @@ provider "github" {
   token = var.github_token
 }
 
+data "github_repository" "weave_gitops_clusters" {
+  full_name = "weaveworks/weave-gitops-clusters"
+}
+
+resource "github_issue" "test" {
+  repository       = github_repository.weave_gitops_clusters.name
+  title            = "My issue title"
+  body             = "The body of my issue"
+}
